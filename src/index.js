a1f246bef2ec92b344196ee55e7064483327020e
Vienna B <viennabdev@gmail.com>
2022-11-17 14:29:55 +0000

    function to check guesses, change css and reveal word

--- src/index.js
+++ src/index.js
@@ -92,6 +92,32 @@ function isWordValid(word) {
   return dictionary.includes(word);
 }
 
+function revealWord(guess) {
+  const row = state.currentRow;
+
+  for (let i = 0; i < 5; i++) {
+    const box = document.getElementById(`box${row}${i}`);
+    const letter = box.textContent;
+
+    if (letter === state.secret[i]) {
+      box.classList.add("right");
+    } else if (state.secret.includes(letter)) {
+      box.classList.add("wrong");
+    } else {
+      box.classList.add("empty");
+    }
+  }
+
+  const isWinner = state.secret === guess;
+  const isGameOver = state.currentRow === 5;
+
+  if (isWinner) {
+    alert("Congratulations");
+  } else if (isGameOver) {
+    alert(`Better luck next time. The word was ${state.secret}`);
+  }
+}
+
 function startUp() {
   const game = document.getElementById("game");
   drawGrid(game);
