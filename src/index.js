29f8d9454b91821490e62d3cd69d6b6b9f64b409
Vienna B <viennabdev@gmail.com>
2022-11-17 14:11:33 +0000

    outline functions to reg keyboard events

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+const state = {
+  grid: Array(6)
+    .fill()
+    .map(() => Array(5).fill("")),
+  currentRow: 0,
+  currentCol: 0,
+};
+
+function updateGrid() {
+  for (let i = 0; i < state.grid.length; i++) {
+    for (let j = 0; j < state.grid[i].length; j++) {
+      const box = document.getElementById(`box${i}${j}`);
+      box.textContent = state.grid[i][j];
+    }
+  }
+}
+
 function drawBox(container, row, col, letter = "") {
   const box = document.createElement("div");
   box.className = "box";
@@ -21,9 +38,34 @@ function drawGrid(container) {
   container.appendChild(grid);
 }
 
+function registerKeyboardEvents() {
+  document.body.onkeydown = (e) => {
+    const key = e.key;
+    if (key === "Enter") {
+      if (state.currentCol === 5) {
+        const word = getCurrentWord();
+        if (isWordValid(word)) {
+          revealWord(word);
+          state.currentRow++;
+          state.currentCol = 0;
+        } else {
+          alert("Not a valid word!");
+        }
+      }
+    }
+    if (key === "Backspace") {
+    }
+    if (isLetter(key)) {
+    }
+  };
+  updateGrid();
+}
+
 function startUp() {
   const game = document.getElementById("game");
   drawGrid(game);
+
+  registerKeyboardEvents();
 }
 
 startUp();
